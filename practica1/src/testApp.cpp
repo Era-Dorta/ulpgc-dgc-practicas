ad33e8d91f350f9850c8acc1711fd1845b2ed538
Neochuky <garoe@portatil-centrino.(none)>
2013-01-31 13:55:02 +0000

    Create and draw a cube

--- practica1/src/testApp.cpp
+++ practica1/src/testApp.cpp
@@ -1,6 +1,53 @@
 #include "testApp.h"
 
 //--------------------------------------------------------------
+Cube::Cube(){
+    for( int i = 0; i < 0; i++){
+        vertices[i].x = 0;
+        vertices[i].y = 0;
+        vertices[i].z = 0;
+    }
+}
+
+//--------------------------------------------------------------
+Cube::Cube( Vertex vertex0, Vertex vertex1 ){
+    setVertices(vertex0, vertex1);
+}
+
+//--------------------------------------------------------------
+void Cube::setVertices( Vertex vertex0, Vertex vertex1 ){
+    int side, z;
+    side = abs(vertex0.x - vertex1.x);
+
+    for(z = 0; z < 1; z++){
+        vertices[0 + z*4].x = vertex0.x;
+        vertices[0 + z*4].y = vertex0.y;
+        vertices[0 + z*4].z = vertex0.z + z*side;
+
+        vertices[1 + z*4].x = vertex0.x + side;
+        vertices[1 + z*4].y = vertex0.y;
+        vertices[1 + z*4].z = vertex0.z + z*side;
+
+        vertices[2 + z*4].x = vertex0.x + side;
+        vertices[2 + z*4].y = vertex0.y + side;
+        vertices[2 + z*4].z = vertex0.z + z*side;
+
+        vertices[3 + z*4].x = vertex0.x;
+        vertices[3 + z*4].y = vertex0.y + side;
+        vertices[3 + z*4].z = vertex0.z + z*side;
+
+
+    }
+}
+
+//--------------------------------------------------------------
+void Cube::draw(){
+    for(int i = 0; i < 4; i++){
+        ofLine(vertices[i].x, vertices[i].y, vertices[(i+1)%4].x, vertices[(i+1)%4].y);
+    }
+}
+
+//--------------------------------------------------------------
 void testApp::setup(){
 }
 
@@ -10,7 +57,7 @@ void testApp::update(){
 
 //--------------------------------------------------------------
 void testApp::draw(){
-
+    cube.draw();
 }
 
 //--------------------------------------------------------------
@@ -35,12 +82,19 @@ void testApp::mouseDragged(int x, int y, int button){
 
 //--------------------------------------------------------------
 void testApp::mousePressed(int x, int y, int button){
-
+    if( button == L_MOUSE){
+        pmouse.x = x;
+        pmouse.y = y;
+        pmouse.z = 0;
+    }
 }
 
 //--------------------------------------------------------------
 void testApp::mouseReleased(int x, int y, int button){
-
+    if( button == L_MOUSE){
+        Vertex current(x, y, 0);
+        cube.setVertices( pmouse, current );
+    }
 }
 
 //--------------------------------------------------------------
@@ -54,6 +108,6 @@ void testApp::gotMessage(ofMessage msg){
 }
 
 //--------------------------------------------------------------
-void testApp::dragEvent(ofDragInfo dragInfo){ 
+void testApp::dragEvent(ofDragInfo dragInfo){
 
 }
--- practica1/src/testApp.h
+++ practica1/src/testApp.h
@@ -2,8 +2,37 @@
 
 #include "ofMain.h"
 
+#define L_MOUSE 0
+#define R_MOUSE 3
+struct Vertex {
+    int x;
+    int y;
+    int z;
+
+    Vertex() { x = 0; y = 0; z = 0;};
+    Vertex( int x_, int y_, int z_ ) { x = x_; y = y_; z = z_;};
+};
+
+typedef struct Vertex Vertex;
+
+class Cube {
+
+    private:
+
+    Vertex vertices[8];
+
+    public:
+    Cube();
+    Cube( Vertex vertex0, Vertex vertex1 );
+    void setVertices( Vertex vertex0, Vertex vertex1 );
+    void draw();
+};
+
 class testApp : public ofBaseApp{
 
+    private:
+        Cube cube;
+        Vertex pmouse;
 	public:
 		void setup();
 		void update();
@@ -18,5 +47,7 @@ class testApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
-		
+
 };
+
+
