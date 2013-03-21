566bf4a210eb55bd54e7389588bed195e56da591
neochuky <neochuki@gmail.com>
2013-03-21 18:50:19 +0000

    Corregido error para permitir modificar la posicion de la fuente de luz

--- practica1/src/lightSource.cpp
+++ practica1/src/lightSource.cpp
@@ -8,11 +8,6 @@ LightSource::LightSource( const ofColor color_ )
     drawTriangles_ = true;
     DrawableObject::resetTransMatrix();
     DrawableObject::resetAuxMatrix();
-    for( int i = 0; i < totalVertices; i++){
-        vertices[i].setX( 0 );
-        vertices[i].setY( 0 );
-        vertices[i].setZ( 0 );
-    }
     totalTriangles = 0;
     triangles = NULL;
 }
@@ -40,12 +35,15 @@ void LightSource::setVertex( const Vertex &vertex ){
     vertices[2].setX( vertices[0].getX() + 10 );
     vertices[3].setY( vertices[0].getY() - 10 );
     vertices[4].setY( vertices[0].getY() + 10 );
+    for( int i = 0; i < totalVertices; i++){
+        transVertices[i] = vertices[i]*transMatrix;
+    }
 }
 
 //--------------------------------------------------------------
 void LightSource::draw(Renderer* const renderer) const{
 
         renderer->setColor(color);
-        renderer->rLine(vertices[1], vertices[2]);
-        renderer->rLine(vertices[3], vertices[4]);
+        renderer->rLine(transVertices[1], transVertices[2]);
+        renderer->rLine(transVertices[3], transVertices[4]);
 }
